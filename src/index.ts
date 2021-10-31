bd0d9c27879d6e34936061ab650e684b151f67d3
smrsan <smrsan@users.noreply.github.com>
2021-10-31 21:04:48 +0330

    Add DB Connection Scripts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import randomStr from "./utils/randomStr";
-
-const len = 4;
-const str = randomStr(len);
-
-console.log("String:", str);
-
-for (let i = 0; ; i++) {
-  if (randomStr(len) === str) {
-    console.log("String found after", i, "tries.");
-    break;
-  }
-}
+import "db";
